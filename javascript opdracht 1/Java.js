05114baa5a3d7877daf499ff7a7161c8164466df
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-11 14:24:08 +0200

    Add files via upload

---
+++ javascript opdracht 1/Java.js
@@ -0,0 +1,10 @@
+console.log("hello Winc Academy")
+
+let firstName='Bram';
+console.log(firstName);
+let Sommetjes=4+4;
+console.log(Sommetjes);
+let VermenigvuldigenDelenAftrekken=56*9/23-17;
+console.log(VermenigvuldigenDelenAftrekken);
+let Age='vijfendertig';
+console.log(Age);
