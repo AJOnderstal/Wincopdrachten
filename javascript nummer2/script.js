579da335df97f3c0b6367add2115152b7c2691a1
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-13 14:32:15 +0200

    Add files via upload

---
+++ javascript nummer2/script.js
@@ -0,0 +1,13 @@
+console.log ("hallo Winc Academy Studenten");
+/*hieronder 2 sommen:
+1 met grote uitkomst
+en 1 met een gedeelde uitkomst
+let vermenigvuldigen=1230941*1823794;
+console.log (vermenigvuldigen);
+let delen=637263 / 54;
+console.log (delen);
+*/
+
+//alles tussen de asterix en de schuine streep wordt uitgezet met een blokcomment kun je dit doen.
+
+
