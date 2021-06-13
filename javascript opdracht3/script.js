56a8632f544560fdb8b825ceef2571428440434d
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-13 16:12:17 +0200

    Add files via upload

---
+++ javascript opdracht3/script.js
@@ -0,0 +1,20 @@
+const Age=21;
+if (Age <18 ){
+    document.write("Helaas je bent te jong om naar binnen te gaan!<br>");
+}else{
+    document.write("Welkom in de kroeg!<br>");
+}
+
+const isFemale=true;
+if (isFemale){
+    document.write("Je kan verder met bestellen voor de kaarten van de ladies Night<br>");
+}else{
+    document.write("Helaas het is Ladies Night dus mannen zijn deze keer niet welkom<br>");
+}
+let bob=false
+const driverStatus=bob;
+if (bob){
+    document.write("ok je mag naar huis rijden wel thuis!<br>");
+}else{
+    document.write("Ik bel een taxi voor je zo kun jij niet meer rijden!<br>");
+}
