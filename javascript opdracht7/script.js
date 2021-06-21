40b5f70146d8f019136dc55e635eab30f20da1cf
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-21 21:06:34 +0200

    Add files via upload

---
+++ javascript opdracht7/script.js
@@ -0,0 +1,7 @@
+const wallPaint = function (_muurkleur, _windrichting){
+    console.log('the '+ _windrichting + ' wall is painted ' + _muurkleur)
+}
+
+
+wallPaint ("green","South-East");
+wallPaint ("orange", "North-West");
