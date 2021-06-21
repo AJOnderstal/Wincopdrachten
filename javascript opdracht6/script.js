ee01ab76a96edd8538b9d520a8d3d465fe476f52
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-21 17:55:21 +0200

    Add files via upload

---
+++ javascript opdracht6/script.js
@@ -0,0 +1,27 @@
+//console.log ('dit is opdracht 5')
+
+const makePizza = function (){
+    console.log ('make the dough');
+    console.log ('roll out the dough');
+    console.log ('put tomato sauce on the dough');
+    console.log ('put on some cheese on the sauce');
+    console.log ('put on fresh ingredients on the sauce');
+    console.log ('put your pizza in the oven and set is on 220 degrees celcius and put the timer on 10 minutes')
+}
+
+const makeSushi = function(){
+    console.log('cook rice')
+    console.log('roll out seawheatpaper')
+    console.log('put salmon on the seawheatpaper')
+    console.log('put rice on the salmon')
+    console.log('roll-up the seawheatpaper')
+    console.log('slice the roll in to even pieces')
+}
+
+
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
