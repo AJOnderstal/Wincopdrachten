58ed39da73b1113fe0db9097f61e974db9c02af1
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-22 21:24:20 +0200

    Add files via upload

--- javascript opdracht9/script.js
+++ javascript opdracht9/script.js
@@ -19,8 +19,29 @@ function wiskunde (number1, number2) {
 */
 
 //arrow function
-function wiskunde  (number1, number2) => const kwadraat1 = number1 * number1; const kwadraat2 = number2 * number2; const som = kwadraat1 + kwadraat2; const kwadraat3 = som * som;
+const wiskunde = (number1, number2) => {
+    const kwadraat1 =  number1 * number1;
+    const kwadraat2 = number2 * number2;
+    const som = kwadraat1 + kwadraat2;
+    const kwadraat3 = som * som;
+    return kwadraat3; 
+};
 
 
 console.log(wiskunde(5, 10));
-//arrow function niet duidelijk hoe maar volgens intro niet van groot belang de function met expression is de meest logische maniet van schrijven.
+
+
+/* kortere manier volgens oplossing:
+const square = function(number) {
+    return number * number;
+};
+
+const doSquareCalculation = function(number1, number2) {
+    return square(square(number1) + square(number2));
+};
+
+onst doSquareCalculation = (number1, number2) => {
+    const sum = number1 * number1 + number2 * number2;
+    return sum * sum;
+};
+*/
