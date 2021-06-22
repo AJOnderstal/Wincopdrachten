fa71e908c47d96a038be2b61fd3d75e2ff5d5d02
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-22 21:18:12 +0200

    Add files via upload

---
+++ javascript opdracht9/script.js
@@ -0,0 +1,26 @@
+
+/*function with an expression
+const wiskunde = function(number1, number2) {
+    const kwadraat1 =  number1 * number1;
+    const kwadraat2 = number2 * number2;
+    const som = kwadraat1 + kwadraat2;
+    const kwadraat3 = som * som;
+    return kwadraat3;
+};
+  */  
+/*function with a declaration
+function wiskunde (number1, number2) {
+    const kwadraat1 =  number1 * number1;
+    const kwadraat2 = number2 * number2;
+    const som = kwadraat1 + kwadraat2;
+    const kwadraat3 = som * som;
+    return kwadraat3; 
+}
+*/
+
+//arrow function
+function wiskunde  (number1, number2) => const kwadraat1 = number1 * number1; const kwadraat2 = number2 * number2; const som = kwadraat1 + kwadraat2; const kwadraat3 = som * som;
+
+
+console.log(wiskunde(5, 10));
+//arrow function niet duidelijk hoe maar volgens intro niet van groot belang de function met expression is de meest logische maniet van schrijven.
