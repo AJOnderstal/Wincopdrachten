a50488f8446f1a4f66f318c2d98ab8c09b04f938
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-24 16:02:41 +0200

    Add files via upload

---
+++ javascript opdracht10/script.js
@@ -0,0 +1,34 @@
+
+/*
+const checking = function (age) {
+    console.log ("checken of age is 18 or higher..")
+    if (age <= 18){true
+         }else{false
+        }    
+};
+
+const Greet = function (greetz) {
+   checking(); 
+    if (checking == true){
+        console.log ("Hello there!")
+    }else{
+        console.log ("Hey Kiddo!")
+    }
+};
+
+console.log (Greet(35));
+*/
+
+//deel 2:
+//A:
+
+const zonderBTW = function (number){
+    return number
+}
+
+const metBTW = function (number){
+    zonderBTW(); number * 0.21
+    return number
+}
+
+console.log (metBTW(50));
