bb2d4b4975fd4a1385b491d4b7ae6dd690a373cd
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-24 16:28:41 +0200

    Add files via upload

--- javascript opdracht10/script.js
+++ javascript opdracht10/script.js
@@ -1,34 +1,54 @@
 
-/*
+
 const checking = function (age) {
-    console.log ("checken of age is 18 or higher..")
-    if (age <= 18){true
-         }else{false
+    if (age <= 18){ 
+        return true
+         }else{
+             return false
         }    
 };
 
-const Greet = function (greetz) {
+const Greet = function (age) {
    checking(); 
-    if (checking == true){
-        console.log ("Hello there!")
+    if (checking(age)){
+       return "Hello there!"
     }else{
-        console.log ("Hey Kiddo!")
+        return "Hey Kiddo!"
     }
 };
 
 console.log (Greet(35));
-*/
+console.log (Greet(15))
+
 
 //deel 2:
 //A:
-
-const zonderBTW = function (number){
-    return number
+/*
+const BTWberekenen = function (basisprijs, btwpercentage){
+    return basisprijs * (btwpercentage / 100)
 }
 
-const metBTW = function (number){
-    zonderBTW(); number * 0.21
-    return number
+const metBTW = function (basisprijs, btwpercentage){
+    const metBTW = BTWberekenen(basisprijs, btwpercentage)
+    return basisprijs + metBTW
 }
 
-console.log (metBTW(50));
+console.log (metBTW(1000, 21));
+console.log (metBTW(2, 9));
+*/
+//deel 2:
+//B:
+
+const BTWterugrekenen = function(prijsmetBTW, btwpercentage){
+    const berekenbaisbedrag = prijsmetBTW / ((100 + btwpercentage) / 100);
+    return berekenbaisbedrag
+};
+
+const berekenprijsenBTW = function(prijsmetBTW, btwpercentage){
+const baisbedrag = BTWterugrekenen(prijsmetBTW, btwpercentage);
+const btw = prijsmetBTW - baisbedrag;
+return [baisbedrag, btw];
+};
+
+console.log(berekenprijsenBTW(1210, 21)); // [1000, 210]
+console.log(berekenprijsenBTW(2.18, 9)); // [2, 0.18]
