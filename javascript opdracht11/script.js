ea5df4b5e71978a990328d83aa7f0d0d5c3dd53e
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-27 11:42:12 +0200

    Add files via upload

---
+++ javascript opdracht11/script.js
@@ -0,0 +1,37 @@
+// while
+/*
+let w = 0;
+while (w < 5) {
+  console.log(`This is while iteration number ${w}`);
+  w++;
+}
+
+// for
+for (let i = 0; i < 5; i++) {
+  console.log(`This is for iteration number ${i}`);
+}
+
+// Expected output:
+// This is while iteration number 0
+// This is while iteration number 1
+// This is while iteration number 2
+// This is while iteration number 3
+// This is while iteration number 4
+// This is for iteration number 0
+// This is for iteration number 1
+// This is for iteration number 2
+// This is for iteration number 3
+// This is for iteration number 4
+*/
+
+
+for (let i = 0; i <= 20; i++) {
+     if ( (i) =){
+        console.log ("oneven")} else{
+            console.log ("even")
+        
+    };
+    
+  }
+
+  console.log ("de nummers t/m 20 zijn nu verdeeld in even en oneven.")
