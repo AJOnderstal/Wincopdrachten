6202cd0ae35c472abff3edb773d696264e12e81a
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-27 12:33:14 +0200

    Add files via upload

---
+++ javascript opdracht13/script.js
@@ -0,0 +1,11 @@
+function myFunction1() {
+    var person = prompt("Wat is je naam?", "Pietje precies");
+    if (person != null) {
+      document.getElementById("demo").innerHTML =
+      "Hallo " + person + "!";
+    }
+  };
+
+function myFunction2(){
+    
+}
