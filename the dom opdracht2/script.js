a7cb4ea31592eaa660d736f32a3415890e2e58dd
AJOnderstal <84046795+AJOnderstal@users.noreply.github.com>
2021-06-29 18:17:19 +0200

    Add files via upload

---
+++ the dom opdracht2/script.js
@@ -0,0 +1,28 @@
+/*
+const alertButton = document.getElementById("mybutton");
+alertButton.addEventListener("click", function () {
+  alert("button clicked!");
+});
+
+*/
+const changeClassBlueBackground = function () {
+    const bodyElement = document.body;
+    bodyElement.classList.add("red-background");
+  };
+  const attachEventToChangeColorButton = function () {
+    const changeColorButton = document.getElementById("change-background-button");
+    changeColorButton.addEventListener("click", function () {
+      changeClassBlueBackground();
+    });
+  };
+  const attachEventToAlertButton = function () {
+    const alertButton = document.getElementById("alert-button");
+    alertButton.addEventListener("click", function () {
+      alert("Hello World!");
+    });
+  };
+  
+  attachEventToAlertButton();
+  attachEventToChangeColorButton();
+
+
